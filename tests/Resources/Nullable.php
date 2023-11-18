4a9a7c298b9b68cba22a88140c25527e229cfdac
Tobias Nyholm <tobias.nyholm@gmail.com>
2023-11-18 09:00:44 +0100

    Added test for nullable (#4)
    * Added test for nullable
    
    * cs

---
+++ tests/Resources/Nullable.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Nyholm\ClassRequirementExtractor\Test\Resources;
+
+class Nullable
+{
+    private $noTypeHint;
+    private string $notNullable;
+    private ?string $nullableString;
+}
--- tests/unit/RequirementExtractorTest.php
+++ tests/unit/RequirementExtractorTest.php
@@ -5,6 +5,7 @@ namespace Nyholm\ClassRequirementExtractor\Test\unit;
 use Nyholm\ClassRequirementExtractor\ExtractorFactory;
 use Nyholm\ClassRequirementExtractor\RequirementExtractor;
 use Nyholm\ClassRequirementExtractor\Test\Resources\CreateCompany;
+use Nyholm\ClassRequirementExtractor\Test\Resources\Nullable;
 use Nyholm\ClassRequirementExtractor\Test\Resources\Simple;
 use PHPUnit\Framework\TestCase;
 
@@ -81,5 +82,13 @@ class RequirementExtractorTest extends TestCase
         $this->assertTrue($req['name']->isReadable());
         $this->assertTrue($req['name']->isWriteable());
         $this->assertEquals('string', $req['name']->getTypes()[0]);
+    }
+
+    public function testNullable()
+    {
+        $req = self::$extractor->extract(Nullable::class);
+        $this->assertTrue($req['noTypeHint']->isNullable());
+        $this->assertFalse($req['notNullable']->isNullable());
+        $this->assertTrue($req['nullableString']->isNullable());
     }
 }
